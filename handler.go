2cc78d31aeff98be511f228896663bda781b000c
Arthur White <arthur@whitedevops.com>
2015-08-03 12:32:30 +0200

    New OriginsMap type

--- handler.go
+++ handler.go
@@ -26,6 +26,10 @@ const (
 	headerRequestMethod  = "Access-Control-Request-Method"
 )
 
+// OriginsMap is a list of allowed origins and their respective options.
+type OriginsMap map[string]*Options
+type formattedOriginsMap map[string]*formattedOptions
+
 // Options represents access control options for an origin.
 type Options struct {
 	AllowedHeaders     []string      // AllowedHeaders indicates, in the case of a preflight request, which headers can be used during the actual request. If none are set, all are allowed.
@@ -44,61 +48,72 @@ type formattedOptions struct {
 }
 
 // Use adds a handler that sets CORS with the provided options for all dowstream requests.
-func Use(options map[string]Options) {
-	fmtOpt := formatCORS(options)
+func Use(origins OriginsMap) {
+	fmtOrigins := formatCORS(origins)
 	core.Use(func(c *core.Context) {
-		setCORS(c, fmtOpt, c.Next)
+		setCORS(c, fmtOrigins, c.Next)
 	})
 }
 
 // LocalUse sets CORS with the provided options locally, for a single handler.
 // The global options (if used) are overwritten in this situation.
-func LocalUse(c *core.Context, options map[string]Options, handler func()) {
-	setCORS(c, formatCORS(options), handler)
+func LocalUse(c *core.Context, origins OriginsMap, handler func()) {
+	setCORS(c, formatCORS(origins), handler)
 }
 
-func formatCORS(opt map[string]Options) map[string]formattedOptions {
-	fmtOpt := make(map[string]formattedOptions, len(opt))
-
-	for origin, item := range opt {
-		result := formattedOptions{}
-		if len(item.AllowedHeaders) > 0 {
-			*result.AllowedHeaders = strings.Join(item.AllowedHeaders, ", ")
-		}
-		if len(item.AllowedMethods) > 0 {
-			*result.AllowedMethods = strings.Join(item.AllowedMethods, ", ")
-		}
-		if item.CredentialsAllowed {
-			*result.CredentialsAllowed = "true"
-		}
-		if len(item.ExposedHeaders) > 0 {
-			*result.ExposedHeaders = strings.Join(item.ExposedHeaders, ", ")
-		}
-		if item.MaxAge.Seconds() > 0.5 {
-			*result.MaxAge = fmt.Sprintf("%.f", item.MaxAge.Seconds())
-		}
-
-		fmtOpt[origin] = result
+func formatCORS(origins OriginsMap) formattedOriginsMap {
+	// If no origins map is set, all are allowed.
+	if origins == nil {
+		origins = OriginsMap{AllOrigins: nil}
 	}
 
-	return fmtOpt
+	fmtOrigins := make(formattedOriginsMap)
+
+	for origin, opts := range origins {
+		// If no options are set, all are allowed.
+		if opts == nil {
+			opts = new(Options)
+		}
+
+		fmtOpts := new(formattedOptions)
+
+		if len(opts.AllowedHeaders) > 0 {
+			*fmtOpts.AllowedHeaders = strings.Join(opts.AllowedHeaders, ", ")
+		}
+		if len(opts.AllowedMethods) > 0 {
+			*fmtOpts.AllowedMethods = strings.Join(opts.AllowedMethods, ", ")
+		}
+		if opts.CredentialsAllowed {
+			*fmtOpts.CredentialsAllowed = "true"
+		}
+		if len(opts.ExposedHeaders) > 0 {
+			*fmtOpts.ExposedHeaders = strings.Join(opts.ExposedHeaders, ", ")
+		}
+		if opts.MaxAge.Seconds() != 0 {
+			*fmtOpts.MaxAge = fmt.Sprintf("%.f", opts.MaxAge.Seconds())
+		}
+
+		fmtOrigins[origin] = fmtOpts
+	}
+
+	return fmtOrigins
 }
 
 // setCORS sets the response headers and continues downstream if it's not a preflight request.
-func setCORS(c *core.Context, fmtOpts map[string]formattedOptions, handler func()) {
+func setCORS(c *core.Context, fmtOrigins formattedOriginsMap, handler func()) {
 	origin := c.Request.Header.Get("Origin")
 
 	// Use CORS only if an Origin header is defined for the request.
 	if origin != "" {
-		fmtOpt, knownOrigin := fmtOpts[origin]
+		fmtOpts, knownOrigin := fmtOrigins[origin]
 		allOriginsAllowed := false
 
-		// Unknown origin: check for wildcard.
+		// If origin is unknown, see for wildcard.
 		if !knownOrigin {
-			fmtOpt, allOriginsAllowed = fmtOpts[AllOrigins]
+			fmtOpts, allOriginsAllowed = fmtOrigins[AllOrigins]
 		}
 
-		// No origin matched and wildcard not accepted: reject the request.
+		// If origin is unknown and wildcard isn't set, reject the request.
 		if !knownOrigin && !allOriginsAllowed {
 			http.Error(c.ResponseWriter, "Invalid CORS request", http.StatusForbidden)
 			return
@@ -108,33 +123,33 @@ func setCORS(c *core.Context, fmtOpts map[string]formattedOptions, handler func(
 		c.ResponseWriter.Header().Set("Vary", "Origin")
 
 		// Set credentials header only if they are allowed.
-		if fmtOpt.CredentialsAllowed != nil {
-			c.ResponseWriter.Header().Set(headerAllowCredentials, *fmtOpt.CredentialsAllowed)
+		if fmtOpts.CredentialsAllowed != nil {
+			c.ResponseWriter.Header().Set(headerAllowCredentials, *fmtOpts.CredentialsAllowed)
 		}
 
-		if fmtOpt.ExposedHeaders != nil {
-			c.ResponseWriter.Header().Set(headerExposeHeaders, *fmtOpt.ExposedHeaders)
+		if fmtOpts.ExposedHeaders != nil {
+			c.ResponseWriter.Header().Set(headerExposeHeaders, *fmtOpts.ExposedHeaders)
 		}
 
-		if fmtOpt.MaxAge != nil {
-			c.ResponseWriter.Header().Set(headerMaxAge, *fmtOpt.MaxAge)
+		if fmtOpts.MaxAge != nil {
+			c.ResponseWriter.Header().Set(headerMaxAge, *fmtOpts.MaxAge)
 		}
 
 		// OPTIONS method is used for a preflight request.
 		// In this case, other CORS headers still need to be set before sending all of them, without any other work.
 		if c.Request.Method == "OPTIONS" {
 			// If no allowed headers are set, all are allowed.
-			if fmtOpt.AllowedHeaders == nil {
+			if fmtOpts.AllowedHeaders == nil {
 				c.ResponseWriter.Header().Set(headerAllowHeaders, c.Request.Header.Get(headerRequestHeaders))
 			} else {
-				c.ResponseWriter.Header().Set(headerAllowHeaders, *fmtOpt.AllowedHeaders)
+				c.ResponseWriter.Header().Set(headerAllowHeaders, *fmtOpts.AllowedHeaders)
 			}
 
 			// If no allowed methods are set, all are allowed.
-			if fmtOpt.AllowedMethods == nil {
+			if fmtOpts.AllowedMethods == nil {
 				c.ResponseWriter.Header().Set(headerAllowMethods, c.Request.Header.Get(headerRequestMethod))
 			} else {
-				c.ResponseWriter.Header().Set(headerAllowMethods, *fmtOpt.AllowedMethods)
+				c.ResponseWriter.Header().Set(headerAllowMethods, *fmtOpts.AllowedMethods)
 			}
 
 			c.ResponseWriter.WriteHeader(http.StatusOK)
